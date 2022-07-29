ce9aa4f19e4414bd0b6361f0caf076ac870903cd
scripter4863 <110077361+scripter4863@users.noreply.github.com>
2022-07-29 09:59:13 -0600

    Add files via upload

---
+++ GameScript.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 189707 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/scripter4863/NaturalDisaster/main/NaturalDisaster.lua',true))()
+end
+
+if game.PlaceId == 7991339063 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/rainbow-friends/main/Protected%20(1).lua'),true))()
+end
+
+if game.PlaceId == 4003872968 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/Giant-Survival/main/Giant%20Survival.lua'),true))()
+end
