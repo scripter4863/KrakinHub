5e88c58e1487a750163447fef94802258ef5b273
scripter4863 <110077361+scripter4863@users.noreply.github.com>
2022-08-11 12:14:35 -0600

    Update GameScript.lua

--- GameScript.lua
+++ GameScript.lua
@@ -1,15 +1,13 @@
 if game.PlaceId == 189707 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/NaturalDisaster/main/NaturalDisaster.lua'),true))()
-end
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/scripter4863/NaturalDisaster/main/NaturalDisaster.lua',true))()
 
-if game.PlaceId == 7991339063 then
+elseif game.PlaceId == 7991339063 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/rainbow-friends/main/Protected%20(1).lua'),true))()
-end
 
-if game.PlaceId == 4003872968 then
+elseif game.PlaceId == 4003872968 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/Giant-Survival/main/Giant%20Survival.lua'),true))()
-end
 
-if game.PlaceId == 662417684 then
+elseif game.PlaceId == 662417684 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/scripter4863/LuckyBlocks/main/LuckyBlocks.lua'),true))()
+    
 end
